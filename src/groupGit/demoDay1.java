e4412c0060875ed7a467e620eff140e486590038
MahwishRehman <mahwishrehman88@gmail.com>
2024-08-29 14:27:07 -0400

    first commit

---
+++ src/groupGit/demoDay1.java
@@ -0,0 +1,18 @@
+package groupGit;
+
+public class demoDay1 {
+
+	public static void main(String[] args) {
+
+		
+		//This is a testing Git Project
+		//This is public to collaborate with friends
+		
+		System.out.println("Hello Friends!");
+		
+		System.out.println("Let's work together!");
+		
+		
+	}
+
+}
